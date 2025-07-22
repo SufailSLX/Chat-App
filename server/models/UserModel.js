8aa0ea112655d61db40b4a423037ce73d3e26883
SufailSLX <sufailxd@gmail.com>
2025-07-22 17:06:32 +0530

    Revert "Upated 1.1"
    This reverts commit 248bd6ec1f2e3fdb35f45ab9919262612eeb108e.

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-import { genSalt, hash } from "bcrypt"
+import { genSalt } from "bcrypt"
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -32,18 +32,11 @@ const userSchema = new mongoose.Schema({
     },
 })
 
-// userSchema=pre("save", async function(next){
-//     const salt = await genSalt()
-//     this.password = await hash(this.password, salt)
-//     next()
-// })
-
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next(); // Avoid rehashing if not modified
-    const salt = await genSalt();
-    this.password = await hash(this.password, salt);
-    next();
-});
+userSchema=pre("save", async function(next){
+    const salt = await genSalt()
+    this.password = await hash(this.password, salt)
+    next()
+})
 
 const User = mongoose.model("Users", userSchema)
 
--- server/routes/AuthRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signup } from "../controllers/AuthController.js";
-
-const authRoutes = express.Router();
-
-authRoutes.post("/signup", signup);
-
-export default authRoutes;
- 
